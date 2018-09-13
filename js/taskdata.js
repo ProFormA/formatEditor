c5e32db6caa73864f175e8b91340c3ad2275521a
KBorm <k.borm@ostfalia.de>
2018-09-13 12:18:57 +0200

    read files with XPATH

--- js/taskdata.js
+++ js/taskdata.js
@@ -31,13 +31,39 @@ class XmlReader {
         */
     }
 
-    readSingleText(xpath) {
-        const nodes = this.xmlDoc.evaluate(xpath, this.xmlDoc, this.nsResolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null); // FIRST_ORDERED_NODE_TYPE //ANY_TYPE
+    readSingleText(xpath, node) {
+        const nodes = this.xmlDoc.evaluate(xpath, node?node:this.xmlDoc, this.nsResolver, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
         if (nodes.singleNodeValue)
             return nodes.singleNodeValue.textContent;
         else
             return null;
     }
+
+
+
+    readNodes(xpath) {
+        return this.xmlDoc.evaluate(xpath, this.xmlDoc, this.nsResolver, XPathResult.UNORDERED_NODE_ITERATOR_TYPE, null);
+
+/*        var thisNode = iterator.iterateNext();
+        while (thisNode) {
+            alert( thisNode.textContent );
+            thisNode = iterator.iterateNext();
+        }
+*/
+    }
+
+}
+
+class TaskFile {
+    constructor() {
+        this.filename = '';
+        this.fileclass = '';
+        this.id = null;
+        this.filetype = null;
+        this.comment = null;
+        this.content = null;
+    }
+
 }
 
 class TaskClass {
@@ -47,10 +73,11 @@ class TaskClass {
         this.description = '';
         this.proglang = '';
         this.proglangVersion = '';
+        this.parentuuid = null;
         this.uuid = null;
         this.lang = 'de';
-        this.submissionrestrictions = null;
-        this.parentuuid = null;
+        this.sizeSubmission = 0;
+        this.mimeTypeRegExpSubmission = '';
 
         this.files = [];
         // this.external-resources ;
@@ -62,29 +89,42 @@ class TaskClass {
 
 
     readXml(xmlfile) {
+        try {
 
-       try {
-           let xmlReader = new XmlReader(xmlfile);
+            let xmlReader = new XmlReader(xmlfile);
 
-           this.title = xmlReader.readSingleText("/dns:task/dns:meta-data/dns:title");
-           this.description = xmlReader.readSingleText("/dns:task/dns:description");
-           this.proglang = xmlReader.readSingleText("/dns:task/dns:proglang");
-           this.proglangVersion = xmlReader.readSingleText("/dns:task/dns:proglang/@version");
-           this.uuid = xmlReader.readSingleText("/dns:task/@uuid");
-           this.lang = xmlReader.readSingleText("/dns:task/@lang");
+            this.title = xmlReader.readSingleText("/dns:task/dns:meta-data/dns:title");
+            this.description = xmlReader.readSingleText("/dns:task/dns:description");
+            this.proglang = xmlReader.readSingleText("/dns:task/dns:proglang");
+            this.proglangVersion = xmlReader.readSingleText("/dns:task/dns:proglang/@version");
+            this.uuid = xmlReader.readSingleText("/dns:task/@uuid");
+            this.lang = xmlReader.readSingleText("/dns:task/@lang");
+            this.sizeSubmission = xmlReader.readSingleText("/dns:task/dns:submission-restrictions/dns:regexp-restriction/@max-size");
+            this.mimeTypeRegExpSubmission = xmlReader.readSingleText("/dns:task/dns:submission-restrictions/dns:regexp-restriction/@mime-type-regexp");
 /*
-           alert(this.title);
-           alert(this.description);
-           alert(this.proglang + '/' + this.proglangVersion);
-           alert(this.uuid);
-           alert(this.lang);
+            alert(this.title);
+            alert(this.description);
+            alert(this.proglang + '/' + this.proglangVersion);
+            alert(this.uuid);
+            alert(this.lang);
+            alert(this.sizeSubmission);
+            alert(this.mimeTypeRegExpSubmission);
+
 */
-   /*
-                var result = nodes.iterateNext();
-                while (result) {
-                    txt += result.childNodes[0].nodeValue + "<br>";
-                    result = nodes.iterateNext();
-                }*/
+            // read files
+            let iterator = xmlReader.readNodes("/dns:task/dns:files/dns:file");
+            var thisNode = iterator.iterateNext();
+            while (thisNode) {
+                let taskfile = new TaskFile();
+                taskfile.filename = xmlReader.readSingleText("@filename", thisNode);
+                taskfile.fileclass = xmlReader.readSingleText("@class", thisNode);
+                taskfile.id = xmlReader.readSingleText("@id", thisNode);
+                taskfile.filetype = xmlReader.readSingleText("@type", thisNode);
+                taskfile.comment = xmlReader.readSingleText("@comment", thisNode);
+                taskfile.content = thisNode.textContent;
+                this.files[taskfile.id] = taskfile;
+                thisNode = iterator.iterateNext();
+            }
 
 
        } catch (err){
@@ -92,6 +132,5 @@ class TaskClass {
            setErrorMessage("Error while parsing the xml file. The file has not been imported.". err);
            return; // Stop. Do not make any further changes.
        }
-
     }
 }
